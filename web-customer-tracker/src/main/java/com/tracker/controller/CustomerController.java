fd6404636cd4a42d185120876784a2ba6c013837
mhabram <mateuszhabram@gmail.com>
2018-06-01 22:22:47 +0200

    fixed spring-mvc-crud-servlet.xml property named jdbcUrl the url. Added CustomerDAO to the controller and jsp file edited.

--- web-customer-tracker/src/main/java/com/tracker/controller/CustomerController.java
+++ web-customer-tracker/src/main/java/com/tracker/controller/CustomerController.java
@@ -1,14 +1,27 @@
 package com.tracker.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.tracker.dao.CustomerDAO;
+import com.tracker.entity.Customer;
+
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+    //Inject the customer dao
+    @Autowired
+    private CustomerDAO customerDAO;
+    
     @RequestMapping("/list")
     public String listCustomer(Model model) {
+        List<Customer> customer = customerDAO.getCustomers();
+        
+        model.addAttribute("customers", customer);
         
         return "customer-list";
     }
