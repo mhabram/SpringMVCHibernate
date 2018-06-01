69ccc358c6a719b3141febf3a58ab594fe77fb8d
mhabram <mateuszhabram@gmail.com>
2018-06-01 21:01:22 +0200

    Added dao and entity

---
+++ web-customer-tracker/src/main/java/com/tracker/dao/CustomerDAOImpl.java
@@ -0,0 +1,35 @@
+package com.tracker.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.tracker.entity.Customer;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO {
+    //Injecting sessionFactory
+    @Autowired
+    private SessionFactory sessionFactory;
+    
+    @Override
+    @Transactional
+    public List<Customer> getCustomers() {
+        //Current hibernate session
+        Session session = sessionFactory.getCurrentSession();
+        
+        //Query
+        Query<Customer> query = session.createQuery("from Customer", Customer.class);
+        
+        //Execute query, get result
+        List<Customer> customers = query.getResultList();
+        
+        return customers;
+    }
+
+}
